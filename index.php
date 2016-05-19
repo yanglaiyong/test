52508c5e9aef01ca5ac0242385b4ca142786ac18
yanglaiyong <yly_best@163.com>
2016-05-19 18:06:11 +0800

    this is a php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class test
+{
+    public $dbl;
+    public $db2;
+
+    function __construct($instance)
+    {
+        $this->$db1 = register('dbs');
+        $this->$db2 = $instance
+    }
+
+    public function index(){
+        echo "hello world";
+    }
+}
